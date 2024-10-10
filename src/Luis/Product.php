280dbc9df156f889f334e29aad2303087035b76d
Programador-Luis <redes.luisgonzalez@gmail.com>
2024-10-10 18:28:46 -0400

    Class Producto creado

---
+++ src/Luis/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Php\Tests\Luis;
+
+class Product
+{
+    private $name;
+    private $price;
+
+    public function __construct(
+        string $name,
+        float $price
+    ) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function applyDiscount($percentage)
+    {
+        if ($percentage < 0 || $percentage > 100) {
+
+            throw new \InvalidArgumentException("El porcentaje no puede ser en negativo o estás colocando un porcentaje mayor a 100");
+        }
+
+        $descount = $this->price * ($percentage / 100);
+
+        $this->price -= $descount;
+    }
+
+    public function finalPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function showInfo(): string
+    {
+        return "El producto  {$this->name} tiene un costo de {$this->price}";
+    }
+}
+
+$producto = new Product("arroz", 100);
+$producto->applyDiscount(10);
+echo $producto->showInfo();
