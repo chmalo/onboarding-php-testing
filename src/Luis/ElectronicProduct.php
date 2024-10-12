4c777c7b70b2f35592a3d87b9b79d3667086a2f9
Programador-Luis <redes.luisgonzalez@gmail.com>
2024-10-12 14:14:32 -0400

    Class de Producto Electrónica realizado

---
+++ src/Luis/ElectronicProduct.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Php\Tests\Luis;
+
+use Php\Tests\Luis\Product;
+
+class ElectronicProduct extends Product
+{
+
+    public function applyDiscountElectronic($percentage): void
+    {
+        $percentage = min($percentage, 20);
+
+        parent::applyDiscount($percentage);
+
+        if ($this->finalPrice() < 100) {
+            throw new \InvalidArgumentException("El precio de los electrónicos no puede ser menor a $100 después del descuento.");
+        }
+
+    }
+
+
+
+}
--- tests/Luis/ProductTest.php
+++ tests/Luis/ProductTest.php
@@ -4,17 +4,31 @@ namespace Php\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Php\Tests\Luis\Product;
- 
- 
+use Php\Tests\Luis\ElectronicProduct;
+
 class ProductTest extends TestCase
 {
     public function testItShouldShowInformation(): void
     {
         $product = new Product("Laptop", 250);
         $showInfo = $product->showInfo();
- 
+
         $this->assertEquals("El producto Laptop tiene un costo de 250", $showInfo);
- 
     }
- 
+
+
+    public function testApplyDiscountElectronic()
+    {
+        $product = new ElectronicProduct("Laptop", 200);
+        $product->applyDiscountElectronic(21);
+        $this->assertEquals(160, $product->finalPrice());
+    }
+
+    public function testPriceDiscountElectronic()
+    {
+        $product = new ElectronicProduct("Laptop", 100);
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("El precio de los electrónicos no puede ser menor a $100 después del descuento.");
+        $product->applyDiscountElectronic(21);
+    }
 }
