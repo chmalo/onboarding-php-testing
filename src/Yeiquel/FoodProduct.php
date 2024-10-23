a9777ac4212bae59b6604946e8c03a4a57af1251
David Cardier <yeiicardier@gmail.com>
2024-10-23 11:38:35 -0400

    Version con Test Yeiquel 6.1

--- src/Yeiquel/FoodProduct.php
+++ src/Yeiquel/FoodProduct.php
@@ -1,27 +1,33 @@
 <?php
-declare(strict_types=1);
 
 namespace Php\Yeiquel;
 
-class FoodProduct extends Product {
+class FoodProduct extends Product
+{
     private string $expirationDate;
 
-    public function __construct(string $name, float $price, string $expirationDate) {
+    private const FIXED_DISCOUNT = 10.0;
+
+    public function __construct(string $name, float $price, string $expirationDate)
+    {
         parent::__construct($name, $price);
         $this->setExpirationDate($expirationDate);
         $this->applyDiscount();
     }
 
-    public function setDiscountPercentage(float $percentage): void {
+    public function setDiscountPercentage(float $percentage): void
+    {
         $this->discountPercentage += $percentage;
     }
 
-    public function applyDiscount(): void {
-        $this->discountPercentage += 10.0;
-        parent::applyDiscount();
+    public function applyDiscount(): void
+    {
+        $this->discountPercentage = self::FIXED_DISCOUNT;
+        $this->applyDiscount();
     }
 
-    public function setExpirationDate(string $expirationDate): void {
+    public function setExpirationDate(string $expirationDate): void
+    {
         if (!$this->validDate($expirationDate)) {
             throw new \InvalidArgumentException("La fecha de expiración no es válida. Debe estar en formato YYYY-MM-DD.");
         }
@@ -33,11 +39,13 @@ class FoodProduct extends Product {
         $this->expirationDate = $expirationDate;
     }
 
-    public function getExpirationDate(): string {
+    public function getExpirationDate(): string
+    {
         return $this->expirationDate;
     }
 
-    private function validDate(string $validDate): bool {
+    private function validDate(string $validDate): bool
+    {
         $date = \DateTime::createFromFormat('Y-m-d', $validDate);
         return $date && $date->format('Y-m-d') === $validDate;
     }
